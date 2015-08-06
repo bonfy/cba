8acb6c8e510f6316c8af46e10a9e43565f382e50
bonfy <foreverbonfy@163.com>
2015-08-06 22:18:46 +0800

    change autohome_trace and bitauto_trace to yield

--- utils.py
+++ utils.py
@@ -7,13 +7,10 @@ from config import USER_AGENTS, logger
 import random
 import datetime
 import json
+from itertools import chain, count
 
-
-#'http://baa.bitauto.com/{model}/index-all-all-{page}-1.html'
-#'http://club.autohome.com.cn/bbs/forum-c-{model}-{page}.html?orderby=dateline&qaType=-1',
 
 def get_content(url):
-
     r = requests.get(url, headers={'User-Agent': random.choice(USER_AGENTS)})
     assert r.status_code == 200
     if r.encoding in ('ISO-8859-1', 'gb2312'):
@@ -23,55 +20,59 @@ def get_content(url):
     return r.text
 
 
-def bitauto_trace(lst, model, page, url_format, deadline):
+def bitauto_trace(model, deadline):
+    URL_FORMAT = 'http://baa.bitauto.com/{model}/index-all-all-{page}-1.html'
 
-    url = url_format.format(page=page, model=model)
-    print url
-    content = get_content(url)
+    for page in count(1):
+        url = URL_FORMAT.format(page=page, model=model)
+        logger.debug('bitauto trace {url}'.format(url=url))
+        content = get_content(url)
+        d = pq(content)
+        rows = d('div.postslist_fist_title').nextAll('div.postslist_xh')
 
-    d = pq(content)
-    rows = d('div.postslist_xh')
+        for row in rows:
+            i = pq(row)
+            title = i('li.bt span').text().strip()
+            href = i('li.bt a').attr('href')
+            author = i('li.zz a').html().strip()
 
-    for row in rows:
-        i = pq(row)
-        title = i('li.bt span').text().strip()
-        href = i('li.bt a').attr('href')
-        author = i('li.zz a').html().strip()
-        published = i('li.zhhf').html().strip()
-        now = datetime.datetime.utcnow()
-        now = now.strftime('%Y-%m-%dT%H:%M:%SZ')
+            now = datetime.datetime.utcnow()
+            published = now.strftime(
+                '%Y-%m-%d') if (':' in i('li.zhhf').html().strip()) else i('li.zhhf').html().strip()
+            now = now.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-        print dict(title=title, href=href, author=author, published=published, insert_dt=now)
-        lst.append(
-            dict(title=title, href=href, author=author, published=published, insert_dt=now))
+            if published < deadline:
+                return
 
-    return autohome_trace(lst, model, page, url_format, deadline)
+            yield dict(title=title, href=href, author=author, published=published, insert_dt=now)
 
 
-def autohome_trace(lst, model, page, url_format, deadline):
-
+def autohome_trace(model, deadline):
+    URL_FORMAT = 'http://club.autohome.com.cn/bbs/forum-c-{model}-{page}.html?orderby=dateline&qaType=-1'
     BASE_URL = 'http://club.autohome.com.cn'
+    for page in count(1):
 
-    url = url_format.format(page=page, model=model)
-    content = get_content(url)
-    d = pq(content)
-    rows = d('dl.list_dl')[1:]
+        url = URL_FORMAT.format(page=page, model=model)
+        logger.debug('autohome trace {url}'.format(url=url))
+        content = get_content(url)
+        d = pq(content)
+        rows = d('dl.bluebg:last').nextAll('dl.list_dl')
 
-    for row in rows:
-        i = pq(row)
-        title = i('dt a').html().strip()
-        href = BASE_URL + i('dt a').attr('href')
-        author = i('dd:eq(0) a').html()
-        published = i('dd:eq(0) span.tdate').html()
-        now = datetime.datetime.utcnow()
-        now = now.strftime('%Y-%m-%dT%H:%M:%SZ')
+        for row in rows:
 
-        if exit_func(published):
-            return lst
-        lst.append(
-            dict(title=title, href=href, author=author, published=published, insert_dt=now))
+            i = pq(row)
+            title = i('dt a').html().strip()
+            href = BASE_URL + i('dt a').attr('href')
+            author = i('dd:eq(0) a').html()
+            published = i('dd:eq(0) span.tdate').html()
+            now = datetime.datetime.utcnow()
+            now = now.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-    return autohome_trace(lst, model, page, url_format, deadline)
+            if published < deadline:
+                return
+
+            yield dict(title=title, href=href, author=author, published=published, insert_dt=now)
+
 
 """
 def exit_func(published, deadline):
@@ -88,7 +89,6 @@ def feed_item(item):
 
 
 def parse_item():
-
     with open('model.json') as f:
         data = json.load(f)
 
